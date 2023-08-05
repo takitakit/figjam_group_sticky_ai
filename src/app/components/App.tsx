aebd22779505428064c9afacb581f316e119badd
Takeshi Uchida <takitakit@gmail.com>
2023-08-05 21:52:15 +0900

    初期コミット

---
+++ src/app/components/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../styles/ui.css';
+import { Config } from "./Config"
+
+function App() {
+  const textbox = React.useRef<HTMLInputElement>(undefined);
+
+  const countRef = React.useCallback((element: HTMLInputElement) => {
+    if (element) element.value = '5';
+    textbox.current = element;
+  }, []);
+
+  const onCreate = () => {
+    const count = parseInt(textbox.current.value, 10);
+    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
+  };
+
+  const onCancel = () => {
+    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+  };
+
+  React.useEffect(() => {
+    // This is how we read messages sent from the plugin controller
+    window.onmessage = (event) => {
+      const { type, message } = event.data.pluginMessage;
+      if (type === 'create-rectangles') {
+        console.log(`Figma Says: ${message}`);
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <Config />
+      <h2>Rectangle Creator</h2>
+      <p>
+        Count: <input ref={countRef} />
+      </p>
+      <button id="create" onClick={onCreate}>
+        Create
+      </button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/Config.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Config:React.FC = () => {
+
+  return (
+    <p>config</p>
+
+  )
+}
